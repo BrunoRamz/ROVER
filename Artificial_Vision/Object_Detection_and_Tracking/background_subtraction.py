c0c5159003755c968b2195c26bbcae108aed5b75
Bruno Ramz <bruce.iwan02@gmail.com>
2023-05-19 14:56:17 -0600

    [UPD]: background subtraction class.

--- Artificial_Vision/Object_Detection_and_Tracking/background_subtraction.py
+++ Artificial_Vision/Object_Detection_and_Tracking/background_subtraction.py
@@ -7,49 +7,46 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+ESC_KEY = int(os.getenv("ESC_KEY"))
+WAIT_KEY = int(os.getenv("WAIT_KEY"))
+SCALING_FACTOR = float(os.getenv("SCALING_FACTOR"))
+DEVICE_INDEX = int(os.getenv("DEVICE_INDEX"))
+HISTORY = int(os.getenv("HISTORY"))
+RATE = float(os.getenv("RATE"))
+
 
 class BackgroundSubtraction:
+    def __init__(self):
+        self.capture = cv2.VideoCapture(DEVICE_INDEX)
+        self.background_subtractor = cv2.createBackgroundSubtractorMOG2()
+        self.learning_rate = RATE/HISTORY
 
-    def get_frame(self, cap):
-        scaling_factor = float(os.getenv("SCALING_FACTOR"))
-        _, self.frame = cap.read()
-        self.resize_image_frame = cv2.resize(
-            self.frame, None, fx=scaling_factor,
-            fy=scaling_factor, interpolation=cv2.INTER_AREA
+    def compute_maks(self, frame):
+        mask = self.background_subtractor.apply(
+            frame, learningRate=self.learning_rate
         )
-
-        return self.resize_image_frame
-
-    def compute_maks(self, background_subtractor, frame, learning_rate):
-        mask = background_subtractor.apply(frame, learningRate=learning_rate)
         grayscale_to_rgb = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
         return grayscale_to_rgb
 
+    def star_background_subtraction(self):
+        while True:
+            _, self.frame = self.capture.read()
+            self.resize_image_frame = cv2.resize(
+                self.frame, None, fx=SCALING_FACTOR,
+                fy=SCALING_FACTOR, interpolation=cv2.INTER_AREA
+            )
+            self.mask = self.compute_maks(self.resize_image_frame)
+            cv2.imshow('Intput', self.resize_image_frame)
+            cv2.imshow('Output', self.mask & self.resize_image_frame)
 
-cap = cv2.VideoCapture(int(os.getenv("DEVICE_INDEX")))
-history = int(os.getenv("HISTORY"))
-rate = float(os.getenv("RATE"))
-esc_key = int(os.getenv("ESC_KEY"))
-wait_key = int(os.getenv("WAIT_KEY"))
+            key = cv2.waitKey(WAIT_KEY)
+            if key == ESC_KEY:
+                break
+
+        self.capture.release()
+        cv2.destroyAllWindows()
+
 
 background_subtraction = BackgroundSubtraction()
-background_subtractor = cv2.createBackgroundSubtractorMOG2()
-
-learning_rate = rate/history
-
-while True:
-    frame = background_subtraction.get_frame(cap)
-    mask = background_subtraction.compute_maks(
-        background_subtractor, frame, learning_rate
-    )
-
-    cv2.imshow('Intput', frame)
-    cv2.imshow('Output', mask & frame)
-
-    key = cv2.waitKey(wait_key)
-    if key == esc_key:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+background_subtraction.star_background_subtraction()
