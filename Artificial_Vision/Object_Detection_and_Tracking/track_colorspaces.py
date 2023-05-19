5c0cd7cd9c242743ba792400e8ff34f64be38b2e
Bruno Ramz <bruce.iwan02@gmail.com>
2023-05-19 14:33:39 -0600

    [UPD]: Refactor tracking color spaces file.

--- Artificial_Vision/Object_Detection_and_Tracking/track_colorspaces.py
+++ Artificial_Vision/Object_Detection_and_Tracking/track_colorspaces.py
@@ -8,47 +8,46 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+ESC_KEY = int(os.getenv("ESC_KEY"))
+WAIT_KEY = int(os.getenv("WAIT_KEY"))
+SCALING_FACTOR = float(os.getenv("SCALING_FACTOR"))
+KERNEL_SIZE = int(os.getenv("KERNEL_SIZE"))
+DEVICE_INDEX = int(os.getenv("DEVICE_INDEX"))
+
 
 class ColorSpaces:
 
-    def get_frame(self, cap):
-        scaling_factor = float(os.getenv("SCALING_FACTOR"))
-        _, self.frame = cap.read()
-        self.resize_image_frame = cv2.resize(
-            self.frame, None, fx=scaling_factor,
-            fy=scaling_factor, interpolation=cv2.INTER_AREA
-        )
+    def __init__(self):
+        self.capture = cv2.VideoCapture(DEVICE_INDEX)
 
-        return self.resize_image_frame
-
-    def convert_hsv(self, frame):
-        kernel_size = int(os.getenv("KERNEL_SIZE"))
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    def convert_hsv(self):
+        hsv = cv2.cvtColor(self.resize_image_frame, cv2.COLOR_BGR2HSV)
         lower = np.array([0, 70, 60])
         upper = np.array([50, 150, 255])
         mask = cv2.inRange(hsv, lower, upper)
-        image_bitwise_and = cv2.bitwise_and(frame, frame, mask=mask)
-        image_media_blurred = cv2.medianBlur(image_bitwise_and, kernel_size)
+        image_bitwise_and = cv2.bitwise_and(
+            self.resize_image_frame, self.resize_image_frame, mask=mask
+        )
+        image_media_blurred = cv2.medianBlur(image_bitwise_and, KERNEL_SIZE)
 
         return image_media_blurred
 
+    def start_tracking_color_spaces(self):
+        while True:
+            _, self.frame = self.capture.read()
+            self.resize_image_frame = cv2.resize(
+                self.frame, None, fx=SCALING_FACTOR,
+                fy=SCALING_FACTOR, interpolation=cv2.INTER_AREA
+            )
+            self.hsv_convertion = self.convert_hsv()
+            cv2.imshow('Output', self.hsv_convertion)
+            cv2.imshow('Input', self.resize_image_frame)
 
-esc_key = int(os.getenv("ESC_KEY"))
-wait_key = int(os.getenv("WAIT_KEY"))
-device_index = int(os.getenv("DEVICE_INDEX"))
+            key = cv2.waitKey(WAIT_KEY)
+            if key == ESC_KEY:
+                break
+        cv2.destroyAllWindows()
+
+
 color_spaces = ColorSpaces()
-cap = cv2.VideoCapture(device_index)
-
-while True:
-
-    frame = color_spaces.get_frame(cap)
-    hsv_convertion = color_spaces.convert_hsv(frame)
-
-    cv2.imshow('Output', hsv_convertion)
-    cv2.imshow('Input', frame)
-
-    key = cv2.waitKey(wait_key)
-    if key == esc_key:
-        break
-
-cv2.destroyAllWindows()
+color_spaces.start_tracking_color_spaces()
